cd373eddceee59d2afdd4fa0e03a874d1b7f184c
Allan <allan.ardines@utp.ac.pa>
2025-12-10 15:05:08 -0500

    Codigo Modificado

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $mensaje = isset($_GET['msg']) ? $_GET['msg'] : "Bienvenido";
     <title>Web de Prueba</title>
 </head>
 <body>
-    <h1>Web</h1>
+    <h1>Web Prueba de Funcionamiento</h1>
 
     <p>Mensaje recibido: <?= $mensaje ?> </p>
 
